03571826626d8c3396c5d8242df622b7338894f8
sleniart <45111316+sleniart@users.noreply.github.com>
2020-04-20 21:17:06 +0200

    FirstCommit

---
+++ Assets/Scripts/SirensEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SirensEffect : MonoBehaviour
+{
+    [SerializeField] Light redLight;
+    [SerializeField] Light blueLight;
+
+    private Vector3 redTemp;
+    private Vector3 blueTemp;
+
+    [SerializeField] int speed;
+
+
+    void Update()
+    {
+        redTemp.y += speed * Time.deltaTime;
+        blueTemp.y -= speed * Time.deltaTime;
+
+        redLight.transform.eulerAngles = redTemp;
+        blueLight.transform.eulerAngles = blueTemp;
+    }
+}
